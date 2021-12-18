047060483e6ba875c4d13e383e3d0e1f47c25845
Dylan Baker <dylanmbaker@yahoo.com>
2021-12-17 19:00:14 -0600

    linked to confirmation

--- src/index.js
+++ src/index.js
@@ -1,26 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 // CSS
 import "./index.css";
 
 // Views
 import App from "./App";
-import Pizza from "./views/Pizza";
-import Confirmation from "./views/Confirmation";
 
 // Components
 import Header from './components/header/Header';
 
 render(
     <Router>
-    <Header />
-        <Switch>
-            <Route exact path='/' component={App} />
-            <Route path='/pizza' component={Pizza} />
-            <Route path='/confirmation' component={Confirmation} />
-        </Switch>
+        <Header />
+        <App />
     </Router>, 
     document.getElementById("root")
 );
--- src/views/Pizza.js
+++ src/views/Pizza.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import * as yup from "yup";
 import formSchema from "../validation/formSchema";
+import { Link } from "react-router-dom";
 
 // CSS
 import "../App.css";
@@ -15,7 +16,7 @@ import OrderSectionTop from "../components/orderSectionTop/OrderSectionTop";
 const initialPizzaValues = {
   name: "",
   size: "",
-  sauce: '',
+  sauce: "",
   topping1: false,
   topping2: false,
   topping3: false,
@@ -28,7 +29,7 @@ const initialPizzaValues = {
 const initialErrors = {
   name: "",
   size: "",
-  sauce: '',
+  sauce: "",
   numOfPizzas: "",
 };
 
@@ -88,173 +89,200 @@ const Order = () => {
   const inputChange = (name, value) => {
     validate(name, value);
     setPizzaValues({ ...pizzaValues, [name]: value });
-};
+  };
 
   useEffect(() => {
-    formSchema
-      .isValid(pizzaValues)
-      .then(valid => setDisabled(!valid))
-  }, [pizzaValues])
+    formSchema.isValid(pizzaValues).then((valid) => setDisabled(!valid));
+  }, [pizzaValues]);
 
   return (
-    <form className="order-container" id="pizza-form" onSubmit={submit}>
-      <div className="order-title">Build Your Own Pizza</div>
-      <img src={pizzaImg} alt="pizza" />
-      <p>Build Your Own Pizza</p>
-      <p>{formErrors.name}</p>
-      <p>{formErrors.size}</p>
-      <p>{formErrors.sauce}</p>
-      <OrderSectionTop title="Your Name" />
-      <div className="order-section-bottom">
-        <input
-          type="text"
-          id="name-input"
-          placeholder="Please enter your name"
-          name="name"
-          value={pizzaValues.name}
-          onChange={change}
-        />
-      </div>
-      <OrderSectionTop title="Choice of Size" subtitle="Required" />
-      <div className="order-section-bottom">
-        <select
-          id="size-dropdown"
-          name="size"
-          value={pizzaValues.size}
-          onChange={change}
-        >
-          <option value="">Select a Size</option>
-          <option value="small">Small</option>
-          <option value="medium">Medium</option>
-          <option value="large">Large</option>
-        </select>
-      </div>
-      <OrderSectionTop title="Choice of Sauce" subtitle="Required" />
-      <div className="order-section-bottom">
-        <label>
-          <input
-            id='red'
-            className="sauce-radio"
-            type="radio"
-            name="sauce"
-            value="Red"
-            checked={pizzaValues.sauce === "Red"}
-            onChange={change}
-          />
-          Original Red
-        </label>
-        <label>
-          <input
-            className="sauce-radio"
-            type="radio"
-            name="sauce"
-            value="Ranch"
-            checked={pizzaValues.sauce === "Ranch"}
-            onChange={change}
-          />
-          Garlic Ranch
-        </label>
-        <label>
-          <input
-            className="sauce-radio"
-            type="radio"
-            name="sauce"
-            value="BBQ"
-            checked={pizzaValues.sauce === "BBQ"}
-            onChange={change}
-          />
-          BBQ Sauce
-        </label>
-        <label>
-          <input
-            className="sauce-radio"
-            type="radio"
-            name="sauce"
-            value="Alfredo"
-            checked={pizzaValues.sauce === "Alfredo"}
-            onChange={change}
-          />
-          Spinach Alfredo
-        </label>
-      </div>
-      <OrderSectionTop title="Add Toppings" subtitle="Choose up to 4" />
-      <div className="order-section-bottom">
-        <div className="order-section-half">
-          <label>
-            <input
-              type="checkbox"
-              name="pepperoni"
-              checked={pizzaValues.pepperoni}
-              onChange={change}
-            />
-            Pepperoni
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              name="sausage"
-              checked={pizzaValues.sausage}
-              onChange={change}
-            />
-            Sausage
-          </label>
-        </div>
-        <div className="order-section-half">
-          <label>
-            <input
-              type="checkbox"
-              name="tomatoes"
-              checked={pizzaValues.tomatoes}
-              onChange={change}
-            />
-            Diced Tomatoes
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              name="olives"
-              checked={pizzaValues.olives}
-              onChange={change}
-            />
-            Black Olives
-          </label>
-        </div>
-      </div>
-      <OrderSectionTop title="Choice of Substitute" subtitle="Choose up to 1" />
-      <div className="order-section-bottom">
-        <label>
-          <input
-            type="checkbox"
-            name="glutenFree"
-            checked={pizzaValues.glutenFree}
-            onChange={change}
-          />
-          Gluten Free Crust (+ $1.00)
-        </label>
-      </div>
-      <OrderSectionTop title="Special Instructions" />
-      <div className="order-section-bottom">
-        <label>
+    <>
+      <form className="order-container" id="pizza-form" onSubmit={submit}>
+        <div className="order-title">Build Your Own Pizza</div>
+        <img src={pizzaImg} alt="pizza" />
+        <p>Build Your Own Pizza</p>
+        <p>{formErrors.name}</p>
+        <p>{formErrors.size}</p>
+        <p>{formErrors.sauce}</p>
+        <OrderSectionTop title="Your Name" />
+        <div className="order-section-bottom">
           <input
             type="text"
-            id="special-text"
-            placeholder="Anything else you'd like to add?"
-            name="special"
-            value={pizzaValues.special}
+            id="name-input"
+            placeholder="Please enter your name"
+            name="name"
+            value={pizzaValues.name}
             onChange={change}
           />
-        </label>
-      </div>
-      <div className="add-to-order">
-        <input
-          type="number"
-          name="numOfPizzas"
-          value={pizzaValues.numOfPizzas}
-          onChange={change}
+        </div>
+        <OrderSectionTop title="Choice of Size" subtitle="Required" />
+        <div className="order-section-bottom">
+          <select
+            id="size-dropdown"
+            name="size"
+            value={pizzaValues.size}
+            onChange={change}
+          >
+            <option value="small">Small</option>
+            <option value="medium">Medium</option>
+            <option value="large">Large</option>
+          </select>
+        </div>
+        <OrderSectionTop title="Choice of Sauce" subtitle="Required" />
+        <div className="order-section-bottom">
+          <label>
+            <input
+              id="red"
+              className="sauce-radio"
+              type="radio"
+              name="sauce"
+              value="Red"
+              checked={pizzaValues.sauce === "Red"}
+              onChange={change}
+            />
+            Original Red
+          </label>
+          <label>
+            <input
+              className="sauce-radio"
+              type="radio"
+              name="sauce"
+              value="Ranch"
+              checked={pizzaValues.sauce === "Ranch"}
+              onChange={change}
+            />
+            Garlic Ranch
+          </label>
+          <label>
+            <input
+              className="sauce-radio"
+              type="radio"
+              name="sauce"
+              value="BBQ"
+              checked={pizzaValues.sauce === "BBQ"}
+              onChange={change}
+            />
+            BBQ Sauce
+          </label>
+          <label>
+            <input
+              className="sauce-radio"
+              type="radio"
+              name="sauce"
+              value="Alfredo"
+              checked={pizzaValues.sauce === "Alfredo"}
+              onChange={change}
+            />
+            Spinach Alfredo
+          </label>
+        </div>
+        <OrderSectionTop title="Add Toppings" subtitle="Choose up to 4" />
+        <div className="order-section-bottom">
+          <div className="order-section-half">
+            <label>
+              <input
+                type="checkbox"
+                name="pepperoni"
+                checked={pizzaValues.pepperoni}
+                onChange={change}
+              />
+              Pepperoni
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                name="sausage"
+                checked={pizzaValues.sausage}
+                onChange={change}
+              />
+              Sausage
+            </label>
+          </div>
+          <div className="order-section-half">
+            <label>
+              <input
+                type="checkbox"
+                name="tomatoes"
+                checked={pizzaValues.tomatoes}
+                onChange={change}
+              />
+              Diced Tomatoes
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                name="olives"
+                checked={pizzaValues.olives}
+                onChange={change}
+              />
+              Black Olives
+            </label>
+          </div>
+        </div>
+        <OrderSectionTop
+          title="Choice of Substitute"
+          subtitle="Choose up to 1"
         />
-        <button id="order-button" disabled={disabled}>Add to Order</button>
-      </div>
-    </form>
+        <div className="order-section-bottom">
+          <label>
+            <input
+              type="checkbox"
+              name="glutenFree"
+              checked={pizzaValues.glutenFree}
+              onChange={change}
+            />
+            Gluten Free Crust (+ $1.00)
+          </label>
+        </div>
+        <OrderSectionTop title="Special Instructions" />
+        <div className="order-section-bottom">
+          <label>
+            <input
+              type="text"
+              id="special-text"
+              placeholder="Anything else you'd like to add?"
+              name="special"
+              value={pizzaValues.special}
+              onChange={change}
+            />
+          </label>
+        </div>
+        <div className="add-to-order">
+          <input
+            type="number"
+            name="numOfPizzas"
+            value={pizzaValues.numOfPizzas}
+            onChange={change}
+          />
+          <Link to='/confirmation'>
+            <button id="order-button" disabled={disabled}>
+              Add to Order
+            </button>
+          </Link>
+        </div>
+      </form>
+      {/* {pizza && <h3><center>Orders</center></h3>}
+        {pizza.map(order => {
+          return (
+            <div className="order-box">
+              <h4>Order for: {order.name}</h4>
+              <p>Size: {order.size}</p>
+              <p>Sauce: {order.sauce}</p>
+              <p>Toppings:
+                <ul>
+                  <li>Pepperoni: {order.pepperoni ? 'Yes' : 'No'}</li>
+                  <li>Sausage: {order.sausage ? 'Yes' : 'No'}</li>
+                  <li>Diced Tomatoes: {order.tomatoes ? 'Yes' : 'No'}</li>
+                  <li>Black Olives: {order.olives ? 'Yes' : 'No'}</li>
+                </ul>
+              </p>
+              <p>Gluten Free: {order.glutenFree ? 'Yes' : 'No'}</p>
+              <p>Special Instructions: {order.special ? order.special : 'N/A'}</p>
+            </div>
+          )
+        })}
+      } */}
+    </>
   );
 };
 
